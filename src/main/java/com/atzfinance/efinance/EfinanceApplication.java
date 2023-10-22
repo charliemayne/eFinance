dc5f5f3d17231f7dbe8667f056a2def70b47f647
charliemayne <114412078+charliemayne@users.noreply.github.com>
2023-10-22 18:54:35 -0400

    First commit

---
+++ src/main/java/com/atzfinance/efinance/EfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.atzfinance.efinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atzfinance/efinance/EfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atzfinance.efinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
