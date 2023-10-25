abe8ab53fa49cbc7eb752d774cb50045e41ab122
charliemayne <114412078+charliemayne@users.noreply.github.com>
2023-10-25 12:25:35 -0400

    layout for landing page, started navbar

---
+++ src/main/java/com/atzfinance/efinance/model/User.java
@@ -0,0 +1,21 @@
+package com.atzfinance.efinance.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
+}
