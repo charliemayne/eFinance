57adba3e9e75efd7b4e0f36f5b1aeb993add10bf
charliemayne <114412078+charliemayne@users.noreply.github.com>
2023-11-10 12:54:46 -0500

    loan app review page has clickable cards for each loan app

--- src/main/java/com/atzfinance/efinance/controller/EFinanceController.java
+++ src/main/java/com/atzfinance/efinance/controller/EFinanceController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
@@ -72,5 +69,10 @@ public class EFinanceController {
         List<LoanApplication> loanApplications = loanApplicationService.getAllPendingLoanApplications();
         model.addAttribute("loanApplications", loanApplications);
         return "review_loan_applications";
+    }
+
+    @GetMapping("/reviewLoans/{loanId}")
+    public String reviewLoan(@PathVariable("loanId") Long loanId, Model model) {
+        return "<h1>Hi buddy</h1>";
     }
 }
