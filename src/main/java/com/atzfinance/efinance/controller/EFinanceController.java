29aff1daeccb204d46a887cf3921a057dbf4ac44
patru <patrushi79@yahoo.com>
2023-12-06 14:12:53 -0500

    Inquiry_3.0

--- src/main/java/com/atzfinance/efinance/controller/EFinanceController.java
+++ src/main/java/com/atzfinance/efinance/controller/EFinanceController.java
@@ -243,6 +243,13 @@ public class EFinanceController {
         return "customer_inquiries";
     }
 
+    @GetMapping("reviewInquiry")
+    public String reviewInquiries(Model model){
+        List<Inquiry> inquiry = inquiryService.getAllPendingInquiry();
+        model.addAttribute("inquiry", inquiry);
+        return "review_Inquiry";
+    }
+
     @GetMapping("/myLoans/payment/{loanId}")
     public String paymentID(@PathVariable("loanId") Long id, Model model) {
         Optional<LoanAccount> loanAccount = loanAccountService.getByID(id);
